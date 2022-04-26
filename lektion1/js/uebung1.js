878e4ac3d00ff6f0db78060056f8a0a06240e00a
MichaelHornung1980 <103416199+MichaelHornung1980@users.noreply.github.com>
2022-04-26 16:11:47 +0200

    Aufgabe Dienstag 26.04.2022

--- lektion1/js/uebung1.js
+++ lektion1/js/uebung1.js
@@ -6,14 +6,14 @@
  * 1) Eine Variable land erstellen
  */
 //ToDo: füge ab hier Deinen Code ein
-let land="Spanien"
+let land = "Spanien"
 
 /**
  * 2) Die Variable ausgeben
  */
 function aufgabe2() {
-  console.debug("Aufgabe 2 ==========================");
-  console.debug(land);
+    console.debug("Aufgabe 2 ==========================");
+    console.debug(land);
 }
 
 /**
@@ -21,41 +21,43 @@ function aufgabe2() {
  */
 //ToDo: füge ab hier Deinen Code ein
 let stadt, berg, fluss
+
 /**
  * 4) Gebe den Variablen Werte
  */
 function aufgabe4() {
-  console.debug("Aufgabe 4 ==========================");
-  //ToDo: füge ab hier Deinen Code ein
-let stadt="Freudenstadt"
-let berg="Mount Everest Schwaben"
-let fluss="Neckar"
-  console.debug(stadt,", ", berg,", ", fluss);
+    console.debug("Aufgabe 4 ==========================");
+    //ToDo: füge ab hier Deinen Code ein
+    let stadt = "Freudenstadt"
+    let berg = "Mount Everest Schwaben"
+    let fluss = "Neckar"
+    console.debug(stadt, ", ", berg, ", ", fluss);
 }
 
 /**
  * 5) Erstelle die Variablen mit verschiedenem Typ
  */
 function aufgabe5() {
-  console.debug("Aufgabe 5 ==========================");
+    console.debug("Aufgabe 5 ==========================");
 
-  //ToDo: füge ab hier Deinen Code ein
-let name="Michael Hornung"
-let alter=42
-let groesseInMeter=1.78
-let amLeben=true
-  console.debug(name);
-  console.debug(alter);
-  console.debug(groesseInMeter);
-  console.debug(amLeben);
+    //ToDo: füge ab hier Deinen Code ein
+    let name = "Michael Hornung"
+    let alter = 42
+    let groesseInMeter = 1.78
+    let amLeben = true
+    console.debug(name);
+    console.debug(alter);
+    console.debug(groesseInMeter);
+    console.debug(amLeben);
 }
 
 /**
  * Hauptfunktion, in der die Aufgaben aufgerufen werden
  */
 function main() {
-  aufgabe2();
-  aufgabe4();
-  aufgabe5();
+    aufgabe2();
+    aufgabe4();
+    aufgabe5();
 }
+
 main();
